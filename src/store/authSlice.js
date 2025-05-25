66d0470557256f1c1856557a2d523de7158cc5a6
para8ox-deb <ak3183476@gmail.com>
2025-05-25 22:02:07 +0530

    Updating BE

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://military-asset-management-backend.vercel.app/';
+const API_URL = 'https://military-asset-management-backend.vercel.app/';
 
 // Configure axios defaults
 axios.defaults.baseURL = API_URL;
